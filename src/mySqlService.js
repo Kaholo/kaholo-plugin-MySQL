0680b8a426f1e4297a45f704be674aea7e70b87e
Amit Alon <amit@kaholo.io>
2021-09-19 20:45:39 +0300

    Fixed error with roles in existing methods, and added new method "Create Role"

--- src/mySqlService.js
+++ src/mySqlService.js
@@ -33,8 +33,8 @@ module.exports = class MySQLService{
 
     static buildSqlCommand(baseCmd, filters, orderBy){
         return `${baseCmd}${filters && filters.length ? `
-WHERE ${filters.map(([name, val]) => `${name} = '${val}`).join(" AND ")}'` : ""}${orderBy && orderBy.length ? `
-ORDER BY ${orderBy.map(([name, val]) => `${name}${val ? " " + val : ""}`).join(", ")}` : ""}`;
+                WHERE ${filters.map(([name, val]) => `${name} = '${val}`).join(" AND ")}'` : ""}${orderBy && orderBy.length ? `
+                ORDER BY ${orderBy.map(([name, val]) => `${name}${val ? " " + val : ""}`).join(", ")}` : ""}`;
     }
 
     async executeQuery({query}, dontConnect=false) {
@@ -82,10 +82,10 @@ ORDER BY ${orderBy.map(([name, val]) => `${name}${val ? " " + val : ""}`).join("
     async getDbSize({db}){
         return this.executeQuery({
             query: `SELECT table_schema 'database',
-ROUND(SUM(data_length + index_length) / 1024 / 1024, 1) 'sizeInMb' 
-FROM information_schema.tables 
-GROUP BY table_schema${db && db !== "*" ? `
-WHERE table_schema='${db}'` : ""}`
+                    ROUND(SUM(data_length + index_length) / 1024 / 1024, 1) 'sizeInMb' 
+                    FROM information_schema.tables 
+                    GROUP BY table_schema${db && db !== "*" ? `
+                    WHERE table_schema='${db}'` : ""}`
         });
     }
 
@@ -94,8 +94,8 @@ WHERE table_schema='${db}'` : ""}`
         const sorters = {'table_schema': "DESC", 'table_name': "DESC"};
         return this.executeQuery({
             query: MySQLService.buildSqlCommand(`SELECT table_schema 'database', table_name 'table', 
-ROUND(((data_length + index_length) / 1024 / 1024), 2) 'sizeInMb' 
-FROM information_schema.TABLES`, 
+                                                ROUND(((data_length + index_length) / 1024 / 1024), 2) 'sizeInMb' 
+                                                FROM information_schema.TABLES`, 
             Object.entries(filters), Object.entries(sorters))
         })
     }
@@ -104,8 +104,8 @@ FROM information_schema.TABLES`,
         if (scope) await this.connect(); // if should grant permissions - than handle connection
         const result = {createUser: await this.executeQuery({
             query: `CREATE USER '${user}'@'localhost'
-IDENTIFIED BY '${pass}'${changePass ? " PASSWORD EXPIRE" : ""}${role ?
-`DEFAULT ROLE ${role}` : ""}`
+                    IDENTIFIED BY '${pass}'${changePass ? " PASSWORD EXPIRE" : ""}${role ?
+                    `DEFAULT ROLE ${role}` : ""}`
         }, scope)};
         if (!scope) return result;
         try {
@@ -131,6 +131,27 @@ IDENTIFIED BY '${pass}'${changePass ? " PASSWORD EXPIRE" : ""}${role ?
                 scope == "write" ? "INSERT, DELETE, UPDATE" : "SELECT"} ON ${db}.${table} TO '${user}'@'localhost'`
         }, dontConnect);
     }
+
+    async createRole({role, db, table, scope}){
+        if (!role) throw "Must provide role name!"; 
+        if (!scope && (db || table)) throw "Must provide permission scope!";
+        if (!db && table) throw "If provided specific table, must specify the db it's in";
+        table = table || "*"; db = db || "*";
+        
+        await this.connect();
+        const result = {};
+        try {
+            result.createRole = await this.executeQuery({query: `CREATE ROLE '${role}'@'localhost';`}, true);
+            if (scope) result.grantPermissions = await this.grantPermissions({user: role, db, table, scope}, true);
+        }
+        catch (error) {
+            throw {...result, error};
+        }
+        finally {
+            await this.disconnect();
+        }
+        return result;
+    }
     
     async listDbs({query}){
         return this.executeQuery({
@@ -141,18 +162,17 @@ IDENTIFIED BY '${pass}'${changePass ? " PASSWORD EXPIRE" : ""}${role ?
     async listTables({db, query}){
         return this.executeQuery({
             query: `SELECT table_schema 'database', table_name 'table'
-FROM information_schema.tables WHERE table_type = 'BASE TABLE'${query ? ` AND table_name LIKE '%${query}%'` : ""}
-AND table_schema ${db && db != "*" ? `='${db}'` : "not in ('information_schema','mysql','performance_schema','sys')"}
-ORDER BY table_schema, table_name;`
+                    FROM information_schema.tables WHERE table_type = 'BASE TABLE'${query ? ` AND table_name LIKE '%${query}%'` : ""}
+                        AND table_schema ${db && db != "*" ? `='${db}'` : "not in ('information_schema','mysql','performance_schema','sys')"}
+                    ORDER BY table_schema, table_name;`
         });
     }
     
     async listRoles(){
         return this.executeQuery({
-            query: `SELECT DISTINCT u.User 'roleName', if(e.from_user is NULL,0, 1) active
-FROM mysql.user u
-LEFT JOIN mysql.role_edges e ON e.from_user=u.user
-WHERE u.account_locked='Y' AND u.password_expired='Y' AND u.authentication_string='';`
+            query: `SELECT DISTINCT u.User 'roleName'
+                    FROM mysql.user u
+                    WHERE u.account_locked='Y' AND u.password_expired='Y' AND u.authentication_string='';`
         });
     }
     
